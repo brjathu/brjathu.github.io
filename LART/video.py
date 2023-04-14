7df601248087915b5e1266241664de7c3e34c5b6
Jathushan Rajasegaran <brjathu@gmail.com>
2023-04-14 13:37:20 -0700

    action paper

--- LART/video.py
+++ LART/video.py
@@ -26,54 +26,9 @@ for mp4_file in mp4_files:
         # save a low quality image, highly compressed
         cv2.imwrite(video_src.replace('.mp4', '.jpeg'), image, [cv2.IMWRITE_JPEG_QUALITY, 10])     # save frame as JPEG file    
 
+    os.system(f"/Users/jathushan/Downloads/ffmpeg -y -i {video_src} -b:v 500k -crf 36 {video_src.replace('.mp4', '_2.mp4')}")
+
 # Add the videos HTML to the page
 print(videos_html)
 
 
-
-
-
-
-#  <div>
-#           <div class='row'>
-#             <div class='col'>
-#               <p class='h2'>More Video Results on Kinetics</p>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_Z2t-F5cubpk_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_--yCUKj4Oq4_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_0YqLJjXmU0I_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_OyO-B-omyS0_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_VoYauMky8IQ_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_6KG0mbhejCs_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_7jv141dj_zM_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_ls1fXwwHW3E_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <!-- <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_-gn_eC6qOk0_.mp4', type='video/mp4' autoplay muted loop></video><br> -->
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_5YkINInb29k_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_v3huVwrvWE4_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_9_GQ2Sz20ds_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_-HutuMqTAPw_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <!-- <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_xpFW8m5PZCA_.mp4', type='video/mp4' autoplay muted loop></video><br> -->
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_xtSBE8LFhXY_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_-Y-fUYGcb7o_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <!-- <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val__kcVbo4E2JQ_.mp4', type='video/mp4' autoplay muted loop></video><br> -->
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_-53DvfE42gE_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <!-- <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_4pJ_UNWqLWQ_.mp4', type='video/mp4' autoplay muted loop></video><br> -->
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_H9XWa152whU_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_ZCITJQfuVCY_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <!-- <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_OK2iI2F7KIY_.mp4', type='video/mp4' autoplay muted loop></video><br> -->
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_ySvt7w8irBY_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <!-- <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_lBg84JYUZHM_.mp4', type='video/mp4' autoplay muted loop></video><br> -->
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_B8WQ7foXaJ4_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_5PvvLJAHj-E_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_61oPN1ONIk8_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <!-- <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_sa2rr6ZQiPE_.mp4', type='video/mp4' autoplay muted loop></video><br> -->
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val__oQdPnAcCNs_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val__3l3WdE36TI_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_DxCF_5q1ReU_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <!-- <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_-6ykAmBXIr0_.mp4', type='video/mp4' autoplay muted loop></video><br> -->
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val__AnTe0ywuw0_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <!-- <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_--wYHtYMK9o_.mp4', type='video/mp4' autoplay muted loop></video><br> -->
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_2KQ1mvI_k2Y_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#               <video width='100%' class='video-results' src='files/videos_kinetics/kinetics-val_Ls5n1KFnpeU_.mp4', type='video/mp4' autoplay muted loop></video><br>
-#             </div>
-#           </div>
-#         </div>
